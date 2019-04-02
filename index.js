215cfdcad7f8de38eda4207ba7e01e044524ecf9
Sigrún Líf Erlingsdóttir <borgar@Sigrun-Lifs-Air.tskoli.is>
2019-04-02 09:08:59 +0000

    minor changes

--- index.js
+++ index.js
@@ -2,14 +2,16 @@ const cors = require('cors');
 const express = require('express');
 const fetch = require('node-fetch');
 const app = express()
+
+
 app.use(cors()) 
-app.get('/api1', (req, res) => {
+app.get('/plants', (req, res) => {
     fetch("https://trefle.io/api/plants?token=")
     .then(r=>r.json())
     .then(data=>{  
         console.log(data);
         res.json(data)
-    //hér gerum við eitthvað við gögnin okkar fínu
+        
     })
 })
 const port = 3000
