a9477bae62c9e6a0fe2942931eca9c9dc15f4622
HakonArnarS <43438456+HakonArnarS@users.noreply.github.com>
2019-04-02 12:14:52 +0000

    search virkar

--- index.js
+++ index.js
@@ -6,10 +6,11 @@ const app = express()
 
 app.use(cors()) 
 app.get('/plants', (req, res) => {
-    fetch("https://trefle.io/api/plants?token=")
+    console.log(req.query.plant);
+    fetch("https://trefle.io/api/plants?token=VEdRY0JHVDdvaFZZS0o0UW1HVyswZz09&q=" + req.query.plant)
     .then(r=>r.json())
     .then(data=>{  
-        console.log(data);
+        // console.log(data);
         res.json(data)
         
     })
